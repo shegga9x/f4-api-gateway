8d7a58129420f04c215062c08729e33368cc7cb6
shegga <shegga9x@gmail.com>
2025-03-13 19:48:57 +0700

    Initial version of apiGateway generated by generator-jhipster@8.9.0

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.config.AsyncSyncConfiguration;
+import com.mycompany.myapp.config.EmbeddedElasticsearch;
+import com.mycompany.myapp.config.EmbeddedKafka;
+import com.mycompany.myapp.config.EmbeddedSQL;
+import com.mycompany.myapp.config.JacksonConfiguration;
+import com.mycompany.myapp.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = { ApiGatewayApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class }
+)
+@EmbeddedElasticsearch
+@EmbeddedSQL
+@EmbeddedKafka
+public @interface IntegrationTest {
+    // 5s is Spring's default https://github.com/spring-projects/spring-framework/blob/main/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
